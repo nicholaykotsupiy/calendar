ee4b40d08af1a0973819130906cb7334a78919df
jgordaja <yusya.gordeeva@gmail.com>
2021-05-06 18:29:43 +0300

    add tasks in database

---
+++ app/Http/Requests/Task/EditRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Task;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'string|max:255',
+            'allDay' => 'boolean',
+            'dateStart' => 'required|date',
+            'dateEnd' => 'required|date',
+            'timeStart' => 'required|date_format:"H:i"',
+            'timeEnd' => 'required|date_format:"H:i"',
+        ];
+    }
+}
