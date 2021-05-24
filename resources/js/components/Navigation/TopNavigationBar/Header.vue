bb6e19f1556c41064b0585dd65519469e43243fb
Nicholay Kotsupiy <originalrenter@gmail.com>
2021-05-24 18:11:29 +0300

    hobbit

--- resources/js/components/Navigation/TopNavigationBar/Header.vue
+++ resources/js/components/Navigation/TopNavigationBar/Header.vue
@@ -13,7 +13,7 @@
                     <div class="dropdown-divider"></div>
                     <router-link :to="{name: 'settings.login'}" class="dropdown-item drop_links ">Настройки безопасности</router-link>
                     <div class="dropdown-divider"></div>
-                    <router-link to="#" class="dropdown-item drop_links" @click.prevent="logout">Выход из системы</router-link>
+                    <a to="#" class="dropdown-item drop_links" @click.prevent="logout">Выход из системы</a>
                 </div>
             </div>
         </div>
